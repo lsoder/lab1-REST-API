5ff7dfd8d652528994d285a1c7738808bca2bca6
lsoder <lindaevasoder@gmail.com>
2021-04-08 15:38:21 +0200

    setup server rest file

--- server.js
+++ server.js
@@ -4,7 +4,9 @@ const port = 3000;
 
 app.use(express.json());
 
-
+app.get("/api", (req, res) => {
+    res.json("Välkommen till mitt första API")
+})
 
 app.listen(port, () => {
     console.log ("App is running on port: ", port)
